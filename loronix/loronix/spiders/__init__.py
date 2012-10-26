42b247aa07a3b3148aaf40988cab2bfb9a07e454
Guilhem Vellut <guilhem.vellut@gmail.com>
2012-10-26 23:43:44 +0200

    Modified to correct structure

--- loronix/loronix/spiders/__init__.py
+++ loronix/loronix/spiders/__init__.py
@@ -4,20 +4,3 @@
 # your spiders.
 
 
-from scrapy.contrib.spiders import CrawlSpider, Rule
-from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
-
-class LoronixSpider(CrawlSpider):
-   name = "loronix"
-   allowed_domains = ["wordpress.com"]
-   start_urls = [
-       "http://orfaosdoloronix.wordpress.com/"
-       ]
-   rules = [Rule(SgmlLinkExtractor(allow=[r'orfaosdoloronix.wordpress.com/\d+/\d+/$']), follow=True), 
-	Rule(SgmlLinkExtractor(allow=[r'orfaosdoloronix.wordpress.com/\d+/\d+/page/\d+/$']), follow=True), 
-       	Rule(SgmlLinkExtractor(allow=[r'orfaosdoloronix.files.wordpress.com/.+\.jpg'], deny_extensions = []), callback='parse_image')]
-
-   def parse_image(self, response):
-	filename = response.url.split("/")[-1]
-	print filename
-        open('covers/' + filename, 'wb').write(response.body)
