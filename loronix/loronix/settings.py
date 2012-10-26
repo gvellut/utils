21920112fc83845072a36576b6c33a3a021158c7
Guilhem Vellut <guilhem.vellut@gmail.com>
2012-10-26 23:24:25 +0200

    Added Scrapy spider for all linked images in orfaosdoloronix.wordpress.com

---
+++ loronix/loronix/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for loronix project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/topics/settings.html
+#
+
+BOT_NAME = 'loronix'
+
+SPIDER_MODULES = ['loronix.spiders']
+NEWSPIDER_MODULE = 'loronix.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'loronix (+http://www.yourdomain.com)'
